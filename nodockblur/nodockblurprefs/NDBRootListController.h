6f8aa1f933394ced0ba28726b8e680136c8f5510
Roxiun <contact@roxiun.tk>
2020-06-24 16:07:38 +0800

    7

---
+++ nodockblur/nodockblurprefs/NDBRootListController.h
@@ -0,0 +1,5 @@
+#import <Preferences/PSListController.h>
+
+@interface NDBRootListController : PSListController
+
+@end
---
+++ nodockblur/nodockblurprefs/NDBRootListController.m
@@ -0,0 +1,13 @@
+#include "NDBRootListController.h"
+
+@implementation NDBRootListController
+
+- (NSArray *)specifiers {
+	if (!_specifiers) {
+		_specifiers = [self loadSpecifiersFromPlistName:@"Root" target:self];
+	}
+
+	return _specifiers;
+}
+
+@end
